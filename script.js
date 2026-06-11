57d858f4716c3ae9b7965df042a5ca22eac09eb7
ishikapandeyy <ishikapandey.workmail@gmail.com>
2026-06-11 23:04:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // complete the given function
 
-function palindrome(str){
-let reversed =str.split ('').reverse('').join('');
-return str ===reversed;
+function palindrome(str) {
+  let lowerStr = str.toLowerCase();
+  let reversed = lowerStr.split('').reverse().join('');
+  return lowerStr === reversed;
 }
 module.exports = palindrome
